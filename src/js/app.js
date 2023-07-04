663dd771a452a43d1d7956812fd36a8ee1b70e6d
Elena Karsten <e.karstenzv@gmail.com>
2023-07-04 23:38:16 +0800

    update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import CardWidget from './CardWidget';
 import LuhnAlgorithm from './LuhnAlgorithm';
 
 const cardWidget = new CardWidget(document.querySelector('.cards-widget'));
-cardWidget.bindToDOM();
+cardWidget.bindToDom();
 
 const luhnAlgorithm = new LuhnAlgorithm(cardWidget);
 luhnAlgorithm.initialize();
